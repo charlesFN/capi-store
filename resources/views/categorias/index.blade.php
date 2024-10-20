84030040d3856b41acd75d1ec18311bbe3dcc3be
charlesNF <charles.comercial.nunes@gmail.com>
2024-10-20 02:13:03 -0300

    card container

--- resources/views/categorias/index.blade.php
+++ resources/views/categorias/index.blade.php
@@ -8,38 +8,42 @@
 
         <a href="{{ route('categorias.create') }}" class="btn btn-success"><i class="fas fa-plus mr-2"></i><span>Criar Categoria</span></a>
     </h3>
-    <div class="table-responsive">
-        <table class="table table-hover">
-            <thead>
-                <th>Categoria</th>
-                <th>Qtd. Produtos</th>
-                <th>Opções</th>
-            </thead>
-            <tbody>
-                @forelse ($categorias as $categoria)
-                    <tr>
-                        <td>{{ $categoria->nome_categoria }}</td>
-                        <td>{{ count($categoria->produtos) }}</td>
-                        <td class="d-flex">
-                            <a href="{{ route('categorias.show', ['categoria' => $categoria]) }}" class="btn btn-info"><i class="fas fa-eye"></i></a>
-                            <a href="{{ route('categorias.edit', ['categoria' => $categoria]) }}" class="btn btn-warning mx-2"><i class="fas fa-edit"></i></a>
-                            <form action="{{ route('categorias.delete', ['categoria' => $categoria]) }}" method="post">
-                                @csrf
-                                @method('DELETE')
+    <div class="card">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-hover">
+                    <thead>
+                        <th>Categoria</th>
+                        <th>Qtd. Produtos</th>
+                        <th>Opções</th>
+                    </thead>
+                    <tbody>
+                        @forelse ($categorias as $categoria)
+                            <tr>
+                                <td>{{ $categoria->nome_categoria }}</td>
+                                <td>{{ count($categoria->produtos) }}</td>
+                                <td class="d-flex">
+                                    <a href="{{ route('categorias.show', ['categoria' => $categoria]) }}" class="btn btn-info"><i class="fas fa-eye"></i></a>
+                                    <a href="{{ route('categorias.edit', ['categoria' => $categoria]) }}" class="btn btn-warning mx-2"><i class="fas fa-edit"></i></a>
+                                    <form action="{{ route('categorias.delete', ['categoria' => $categoria]) }}" method="post">
+                                        @csrf
+                                        @method('DELETE')
 
-                                <button type="submit" class="btn btn-danger"><i class="fas fa-trash"></i></button>
-                            </form>
-                        </td>
-                    </tr>
-                @empty
-                    <tr>
-                        <td colspan="3">Nenhum registro encontrado.</td>
-                    </tr>
-                @endforelse
-            </tbody>
-            <tfoot>
+                                        <button type="submit" class="btn btn-danger"><i class="fas fa-trash"></i></button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="3">Nenhum registro encontrado.</td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                    <tfoot>
 
-            </tfoot>
-        </table>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
     </div>
 @endsection
--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -9,42 +9,46 @@
         <a href="{{ route('produtos.create') }}" class="btn btn-success"><i class="fas fa-plus mr-2"></i><span>Cadastrar Produto</span></a>
     </h3>
 
-    <div class="table-responsive">
-        <table class="table tab-le-hover">
-            <thead>
-                <th>Produto</th>
-                <th>Gênero</th>
-                <th>Categoria</th>
-                <th>Valor (R$)</th>
-                <th>Opções</th>
-            </thead>
-            <tbody>
-                @forelse ($produtos as $produto)
-                    <tr>
-                        <td>{{ $produto->nome_produto }}</td>
-                        <td>{{ $produto->genero }}</td>
-                        <td>{{ $produto->categoria->nome_categoria }}</td>
-                        <td>{{ $produto->valor }}</td>
-                        <td class="d-flex">
-                            <a href="" class="btn btn-info"><i class="fas fa-eye"></i></a>
-                            <a href="{{ route('produtos.edit', ['produto' => $produto]) }}" class="btn btn-warning mx-2"><i class="fas fa-edit"></i></a>
-                            <form action="{{ route('produtos.delete', ['produto' => $produto]) }}" method="post">
-                                @csrf
-                                @method('DELETE')
+    <div class="card">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table tab-le-hover">
+                    <thead>
+                        <th>Produto</th>
+                        <th>Gênero</th>
+                        <th>Categoria</th>
+                        <th>Valor (R$)</th>
+                        <th>Opções</th>
+                    </thead>
+                    <tbody>
+                        @forelse ($produtos as $produto)
+                            <tr>
+                                <td>{{ $produto->nome_produto }}</td>
+                                <td>{{ $produto->genero }}</td>
+                                <td>{{ $produto->categoria->nome_categoria }}</td>
+                                <td>{{ $produto->valor }}</td>
+                                <td class="d-flex">
+                                    <a href="" class="btn btn-info"><i class="fas fa-eye"></i></a>
+                                    <a href="{{ route('produtos.edit', ['produto' => $produto]) }}" class="btn btn-warning mx-2"><i class="fas fa-edit"></i></a>
+                                    <form action="{{ route('produtos.delete', ['produto' => $produto]) }}" method="post">
+                                        @csrf
+                                        @method('DELETE')
 
-                                <button type="submit" class="btn btn-danger"><i class="fas fa-trash"></i></button>
-                            </form>
-                        </td>
-                    </tr>
-                @empty
-                    <tr>
-                        <td>Nenhum registro encontrado.</td>
-                    </tr>
-                @endforelse
-            </tbody>
-            <tfoot>
+                                        <button type="submit" class="btn btn-danger"><i class="fas fa-trash"></i></button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td>Nenhum registro encontrado.</td>
+                            </tr>
+                        @endforelse
+                    </tbody>
+                    <tfoot>
 
-            </tfoot>
-        </table>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
     </div>
 @endsection
