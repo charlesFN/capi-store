1303d2017d858cb398637655ef1ef0f97860394d
charlesNF <charles.comercial.nunes@gmail.com>
2025-04-23 09:38:41 -0300

    views para tratamento de respostas do mercado pago - alpha

--- app/Http/Controllers/VendaController.php
+++ app/Http/Controllers/VendaController.php
@@ -8,16 +8,16 @@ class VendaController extends Controller
 {
     public function pagamento_aprovado()
     {
-        //
+        return view('back-urls.success');
     }
 
     public function pagamento_pendente()
     {
-        //
+        return view('back-urls.pending');
     }
 
     public function erro_pagamento()
     {
-        //
+        return view('back-urls.failure');
     }
 }
